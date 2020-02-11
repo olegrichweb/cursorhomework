ff4ae2fb8040ff0c61f00c3225ff1cb27a64bb54
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-02-11 22:09:30 +0200

    2-homework-oleg

--- 2-homework-oleg/2-homework-oleg.js
+++ 2-homework-oleg/2-homework-oleg.js
@@ -2,29 +2,28 @@ let n = Number(prompt('Введите стартовое число', 10));
 if(Number.isInteger(n)){
     console.log(n);
 } else {
-    Number(prompt('Это не подходящее число, введите целое число', 10));
+    let badNum = Number(prompt('Это не подходящее число, введите целое число', 10));
 };
 let m = Number(prompt('Введите конечное число', 100));
 if(Number.isInteger(m)){
     console.log(m);
 } else {
-    Number(prompt('Это не подходящее число, введите целое число', 100));
+    let badNum2 = Number(prompt('Это не подходящее число, введите целое число', 100));
 };
-let checkup = confirm('Нужно пропускать четные числа?');
+const checkup = confirm('Нужно пропускать четные числа?');
 if(checkup === true){
     console.log('НУЖНО');
 } else {
     console.log('НЕНУЖНО');
 };
 let resultNum = 0;
-for (let i=n; i<=m; i++)
-if(checkup === true) {
+for (let i=n; i<=m; i++) {
+if(checkup) {
     if(i % 2 === 0) {
-        continue;
+    continue;
+    }
     }
     resultNum += i;
-} else {
-    resultNum +=i;
 }
 console.log(resultNum);
 document.writeln(`Стартовое число: ${n}`)
