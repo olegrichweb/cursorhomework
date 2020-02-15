f4d32abdaeef9a17ca09acf5fc933b33e2a60a66
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-02-15 17:20:58 +0200

    2-homework-oleg

--- 2-homework-oleg/2-homework-oleg.js
+++ 2-homework-oleg/2-homework-oleg.js
@@ -4,11 +4,11 @@ if(Number.isInteger(n)){
 } else {
     let badNum = Number(prompt('Это не подходящее число, введите целое число', 10));
 };
-let m = Number(prompt('Введите конечное число', 100));
+            m = Number(prompt('Введите конечное число', 100));
 if(Number.isInteger(m)){
     console.log(m);
 } else {
-    let badNum2 = Number(prompt('Это не подходящее число, введите целое число', 100));
+            n = Number(prompt('Это не подходящее число, введите целое число', 100));
 };
 const checkup = confirm('Нужно пропускать четные числа?');
 if(checkup === true){
