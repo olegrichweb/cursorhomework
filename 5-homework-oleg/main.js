4cd872bc88a16ffabd3017d5cced85be3be3ae7f
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-02-22 00:14:12 +0200

    5-homework-oleg

--- 5-homework-oleg/main.js
+++ 5-homework-oleg/main.js
@@ -9,3 +9,79 @@ const randomArray = [];
 };
 console.log(`Функция на создание массива случайных чисел: ${getRandomArray(5, 11, 30)}`);
 
+//2.Создайте функцию getModa(...numbers) – которая вычисляет моду всех переданных в неё аргументов. НЕЦЕЛЫЕ ЧИСЛА ИГНОРИРУЮТСЯ
+const countNumbers = (numbers, num) => {
+    return numbers.filter(n => n === num).length;
+}
+function getModa(...numbers) {
+const data = numbers.filter(n => n === Math.trunc(n));
+let maxCount = 0;
+return Array.from(new Set(data)).reduce((acc, cur) => {
+        const curentCount = countNumbers(data, cur);
+        if(curentCount > maxCount) {
+            maxCount = curentCount;
+            return acc = [cur];
+        } else if(curentCount === maxCount) {
+            return acc = [...acc, cur];
+        };
+        return acc;
+    }, []);
+};
+console.log(`Функция "Мода" : ${getModa(1, 1, 1, 5, 7, 5, 2, 1, 7, 8, 4)}`);
+
+
+//3.Создайте функцию getAverage(...numbers) – которая считает среднее арифметическое всех переданных в неё аргументов. НЕЦЕЛЫЕ ЧИСЛА ИГНОРИРУЮТСЯ
+
+function getAverage(...numbers) {
+    const result = numbers.reduce(function(sum, element) {
+       if(Number.isInteger(element)) {
+        return sum + element;
+       };  
+    } );
+    return result/numbers.length;
+};
+console.log(`Функция нахождения среднего арифметического: ${getAverage(6,5,1,7,9,1,5,8,11,22,14,78)}`);
+
+//4. Создайте функцию getMedian(...numbers) – которая считает медиану всех переданных в неё аргументов. НЕЦЕЛЫЕ ЧИСЛА ИГНОРИРУЮТСЯ
+
+function getMedian(...numbers) {
+    let median = 0;
+    let propIndex = 0;
+    const sortedNubers = numbers.sort((a, b) => a - b);
+if(numbers.length % 2 !== 0) {
+    propIndex = Math.trunc(numbers.length / 2);
+    median = numbers[propIndex];
+} else {
+    propIndex = numbers.length / 2;
+    median = (numbers[propIndex - 1] + numbers[propIndex]) / 2;
+    };
+    return median
+};
+console.log(`Функция нахождения медианы чисел: ${getMedian(1, 5, 6, 7, 9, 8, 4, 3, 12)}`);
+
+
+//5. Создайте функцию filterEvenNumbers(...numbers) – которая фильтрует четные числа переданные как аргументы функции
+
+function filterEvenNumbers(...numbers) {
+    const data = numbers.filter(n => n % 2 !== 0);
+    return Array.from(new Set(data));
+};
+console.log(`Функция вывода четных чисел: ${filterEvenNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)}`)
+
+//6. Создайте функцию countPositiveNumbers(...numbers) – которая посчитает количество числел больших 0
+
+function countPositiveNumbers(...numbers) {
+    const data = numbers.filter(n => n > 0);
+    return data.reduce((sum, cur) => {
+        return sum + cur;
+}, 0);
+};
+console.log(`Функция подсчета суммы чисел, которые больше нуля: ${countPositiveNumbers(-1, 2, 3, 0, -7, -4, -10, 8, -9, 11)}`);
+
+document.writeln(`<p>Результаты функций:</p>
+<p>Функция на создание массива случайных чисел: ${getRandomArray(5, 11, 30)}</p>
+<p>Функция "Мода" : ${getModa(1, 1, 1, 5, 7, 5, 2, 1, 7, 8, 4)}</p>
+<p>Функция нахождения среднего арифметического: ${getAverage(6,5,1,7,9,1,5,8,11,22,14,78)}</p>
+<p>Функция нахождения медианы чисел: ${getMedian(1, 5, 6, 7, 9, 8, 4, 3, 12)}</p>
+<p>Функция вывода четных чисел: ${filterEvenNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)}</p>
+<p>Функция подсчета суммы чисел, которые больше нуля: ${countPositiveNumbers(-1, 2, 3, 0, -7, -4, -10, 8, -9, 11)}</p>`);
