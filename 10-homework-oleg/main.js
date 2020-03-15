ff2e5d323c1abc81f3f7f6fc465db926fc0aed9d
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-03-15 18:14:02 +0200

    10-homework-oleg

---
+++ 10-homework-oleg/main.js
@@ -0,0 +1,32 @@
+
+// const soundObj = {
+//     65: new Audio('./audio/Oleg Maestro - KALENDAR.mp3'),
+//     83: new Audio(`./audio/oleg maestro - 95й.mp3`),
+//     68: new Audio(`./audio/Oleg Maestro - Зима, 2013.mp3`),
+//     70: new Audio(`./audio/oleg maestro - олежкин рэп [part 2].mp3`),
+//     71: new Audio(`./audio/oleg maestro - Сияй моё солнце (dirty).mp3`),
+//     72: new Audio(`./audio/oleg maestro 2020 SPECNAZ.mp3`),
+//     74: new Audio(`./audio/oleg_maestro_Снег_кружится_летает.mp3`),
+//     75: new Audio(`./audio/the katakomb clan Gin47%, Женя Где - музыка.mp3`),
+//     76: new Audio(`./audio/Oleg Maestro - Алёна (Огни маяка).mp3`),
+// };
+
+function playSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if(!sound) return;
+        sound.currentTime = 0;
+        sound.play();
+        key.classList.add('playing');
+
+    document.addEventListener('play', e => {
+        if(sound && sound != e.target) {
+            sound.pause();
+        }
+        sound = e.target;
+    },
+    true
+    );
+};
+
+document.addEventListener(`keydown`, playSound);
--- 9-homework-oleg/main.js
+++ 9-homework-oleg/main.js
@@ -7,7 +7,7 @@ function generateBlocks () {
     for(let i=0; i < blockValue; i++) {
         const child_block = document.createElement("div");
         child_block.classList.toggle("child_block")
-        child_block.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+        child_block.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`
         main_block.append(child_block);
     };
     return main_block
