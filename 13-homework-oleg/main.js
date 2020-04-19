29f1685cdb0778a91fa457bf3db46637c561b0ea
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-04-19 16:49:02 +0300

    13-homework-oleg

--- 13-homework-oleg/main.js
+++ 13-homework-oleg/main.js
@@ -1,7 +1,41 @@
-let ivan = {name: `ivan`};
 
 
-let map = new Map();
-map.set(ivan,`hey`);
+function* createIdGenerator() {
+    let i = 0;
+    while(true){
+        yield i++;
+    };
+};
 
-console.log(map);
+const idGenerator = createIdGenerator();
+const str = document.getElementById(`string`)
+const btnGen = document.querySelector(".button-gen");
+btnGen.onclick = function(){
+    return str.innerHTML = `${idGenerator.next().value} number`;
+};
+
+
+const sizeFont = 14;
+const fontGenerator = newFontGenerator(sizeFont);
+function* newFontGenerator(font) {
+    while(true){
+        let size = yield font;
+        if(size === `up`) {
+            ++font;
+        };
+        if(size === `down`) {
+            --font;
+        };
+    };
+};
+
+const upBtn = document.querySelector(".up");
+const downBtn = document.querySelector(`.down`);
+const text = document.querySelector(`.text`);
+upBtn.onclick = function() {
+    return text.style.fontSize = fontGenerator.next(`up`).value + `px`;
+};
+
+downBtn.onclick = function() {
+    return text.style.fontSize = fontGenerator.next(`down`).value + `px`;
+};
